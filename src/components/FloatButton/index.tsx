9289580492241ddc3dea5ac7d1fe75db14346b56
Josias Furtado <jo.junior94@gmail.com>
2020-10-25 19:45:08 -0300

    fix float btn

--- src/components/FloatButton/index.tsx
+++ src/components/FloatButton/index.tsx
@@ -13,7 +13,7 @@ interface FloatButtonProps {
 const FloatButton: React.FC<FloatButtonProps> = ({ style, onPress, title }) => {
     const { navigate } = useNavigation()
     return (
-        <TouchableOpacity onPress={onPress ? onPress : navigate('NewFeedback')} style={[tailwind(`absolute shadow-md bottom-0 right-0 mr-4 mb-20 rounded-full bg-orange-500 h-12 items-center justify-center ${title ? "flex-row px-4" : "w-12"}`), style]} >
+        <TouchableOpacity onPress={onPress ? () => onPress : () => navigate('NewFeedback')} style={[tailwind(`absolute shadow-md bottom-0 right-0 mr-4 mb-20 rounded-full bg-orange-500 h-12 items-center justify-center ${title ? "flex-row px-4" : "w-12"}`), style]} >
             {title && (
                 <Text style={tailwind('text-white text-lg font-bold')}>{title}</Text>
             )}
